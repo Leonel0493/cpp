9b207bbc01b7c567f8a095ad1837a7a9574d2f58
Leonel Rivas <leonrivas04@gmail.com>
2021-01-24 18:15:57 -0600

    Upgrade al codigo del video de YouTube

--- Matrices/matrices.cpp
+++ Matrices/matrices.cpp
@@ -3,36 +3,34 @@
 #include <time.h>
 
 using namespace std;
-
-int main(){
-    int i, j, opt = 0, cols = 0, rows = 0;
+int main()
+{
+    int i, j, opt = 0, col = 0, row = 0;
 
     do
     {
         system("cls");
-        cout<<"Ingrese el numero de columnas de la matriz: ";
-        cin>>cols;
-
-        cout<<"Ingrese el numero de filas de la matriz: ";
-        cin>>rows;
+        
+        cout<<"Ingrese el numero de columnas: ";
+        cin>>col;
+        cout<<"Ingrese el numero de filas: ";
+        cin>>row;
 
         srand(time(NULL));
-        int arr[rows][cols];
+        int arr[row][col];
 
-        for (i = 0; i < rows; i++)
+        for (i = 0; i < row; i++)
         {
-            for (j = 0; j < cols; j++)
+            for (j = 0; j < col; j++)
             {
-                arr[i][j] = rand()%20;
+                arr[i][j] = rand()%15;
                 cout<<"\t"<<arr[i][j];
             }
             cout<<"\n\n";
         }
-        cout<<"\n\nDesea ejecutar el programa nuevamente? <1- SI || 2- NO>";
+        cout<<"\nDesea generar una nueva matriz? <<1- SI || 2- NO>>";
         cin>>opt;
-
     } while (opt != 2);
-    
     system("pause");
     return 0;
-}
+} //end main
